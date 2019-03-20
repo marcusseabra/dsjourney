c1135e62126ff15be7257d9694deff5d059b7fc4
Marcus <seabra.marcus@gmail.com>
2019-03-20 06:00:58 -0300

    Complemento ao feceting e estudo de funcoes que permitem avaliacao da media e mediana

--- eda/r-pgr/Faceting.R
+++ eda/r-pgr/Faceting.R
@@ -27,3 +27,35 @@ sp + facet_grid(sex ~ day)
 # A função facet_wrap() efetua o faceting e estabelece o número de colunas
 
 sp + facet_wrap(. ~ day, ncol = 2)
+
+##############################################################################################
+
+setwd('/home/seabra/dev/dsjourney-old/EDA/lesson3')
+
+pf <- read.csv('pseudo_facebook.tsv', sep = '\t') 
+
+library(ggplot2) 
+
+qplot(x = friend_count, data = pf) +
+  scale_x_continuous(limits = c(0, 1000))
+
+qplot(x = friend_count, data = pf, xlim = c(0, 1000))
+
+str(pf)
+
+qplot(x = friend_count, data = pf, binwidth = 25) +
+  scale_x_continuous(limits = c(0,1000), breaks = seq(0, 1000, 100)) +
+  facet_grid(~gender)
+
+# Removendo registros em que o gênero não foi informado (na)
+qplot(x = friend_count, data = subset(pf, !is.na(gender)), binwidth = 25) +
+  scale_x_continuous(limits = c(0,1000), breaks = seq(0, 1000, 100)) +
+  facet_grid(~gender)
+
+table(pf$gender)
+
+by(pf$friend_count, pf$gender, summary)
+
+# Em situações em que o histograma apresenta 'cauda longa', a mediana é uma medida mais robusta
+# que a média porque esta característica indica que há valores puxando a média para um valor 
+# mais alto (ou mais baixo)
