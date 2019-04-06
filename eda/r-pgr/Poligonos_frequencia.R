802473432074822348c42f39a3e86d828330d946
Marcus <seabra.marcus@gmail.com>
2019-04-05 21:55:24 -0300

    Finalizando parte teorica de analise de uma variavel

--- eda/r-pgr/Poligonos_frequencia.R
+++ eda/r-pgr/Poligonos_frequencia.R
@@ -2,6 +2,8 @@ setwd('/home/seabra/dev/dsjourney/eda/data')
 
 pf <- read.csv('pseudo_facebook.tsv', sep = '\t') 
 
+library(ggplot2) 
+
 # Histograma
 ggplot(aes(x = friend_count), data = subset(pf, !is.na(gender))) + 
   geom_histogram() + 
@@ -35,4 +37,21 @@ by(pf$www_likes, pf$gender, sum)
 # https://flowingdata.com/2008/02/15/how-to-read-and-use-a-box-and-whisker-plot/
 # https://en.wikipedia.org/wiki/Interquartile_range
 # https://en.wikipedia.org/wiki/File:Boxplot_vs_PDF.svg
+
+# Boxplots
+
+ggplot(aes(x = gender, y = friend_count), data = subset(pf, !is.na(gender))) + 
+  geom_boxplot() +
+  scale_y_continuous(limits = c(0, 1000))
+
+# Segundo o instrutor, o uso de scale_y_continuous pode remover dados e por isso recomenda
+# o uso de coord_cartesian
+ggplot(aes(x = gender, y = friend_count), data = subset(pf, !is.na(gender))) + 
+  geom_boxplot() +
+  coord_cartesian(ylim = c(0, 1000))
   
+ggplot(aes(x = gender, y = friend_count), data = subset(pf, !is.na(gender))) + 
+  geom_boxplot() +
+  coord_cartesian(ylim = c(0, 250))
+
+by(pf$friend_count, pf$gender, summary)
