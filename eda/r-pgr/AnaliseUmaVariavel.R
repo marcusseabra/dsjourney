5ba031f310f49034c14bead12d820ed6509f5b74
Marcus <seabra.marcus@gmail.com>
2019-04-08 04:49:15 -0300

    Exercícios sobre análise de uma variável

--- eda/r-pgr/AnaliseUmaVariavel.R
+++ eda/r-pgr/AnaliseUmaVariavel.R
@@ -1,1 +1,19 @@
-data("diamonds")
+library(ggplot2)
+
+dm <- diamonds
+
+summary(dm)
+str(dm$color)
+
+# Sobre factor variable: https://stats.idre.ucla.edu/r/modules/factor-variables/
+
+levels(dm$color)
+?diamonds
+
+# Histograma da variável preço (price)
+
+ggplot(aes(x = price), data = dm) + 
+  geom_histogram()
+
+summary(dm$price)
+  
