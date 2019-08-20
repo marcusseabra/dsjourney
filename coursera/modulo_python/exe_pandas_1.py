b342b15e6378c6b050d6d634169d7b61588ae8b6
Marcus <seabra.marcus@gmail.com>
2019-08-19 23:14:50 -0300

    Exercicios Pandas

--- coursera/modulo_python/exe_pandas_1.py
+++ coursera/modulo_python/exe_pandas_1.py
@@ -4,7 +4,7 @@ input_dir = '/home/seabra/dev/dsjourney/coursera/modulo_python/data/input'
 
 
 def leitura_arquivo():
-    df = pd.read_csv('olympics.csv', index_col=0, skiprows=1, skipfooter=1)
+    df = pd.read_csv('olympics.csv', index_col=0, skiprows=1, skipfooter=1, engine='python')
     for col in df.columns:
         if col[:2] == '01':
             df.rename(columns={col: 'Gold' + col[4:]}, inplace=True)
@@ -14,9 +14,11 @@ def leitura_arquivo():
             df.rename(columns={col: 'Bronze' + col[4:]}, inplace=True)
         if col[:1] == '№':
             df.rename(columns={col: '#' + col[1:]}, inplace=True)
-    print(df.columns)
+    # print(df.columns)
     # print(df.loc['Gold'].max())
-    df.loc['Gold']
+    serie_most_gold = df.where(df['Gold'] == df['Gold'].max()).dropna()
+    index_most_gold = serie_most_gold.index
+    print(index_most_gold[0])
 
 
 leitura_arquivo()
