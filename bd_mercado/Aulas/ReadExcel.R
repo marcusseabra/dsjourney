818ff413f7c26a8a2c2830468076079a7907d95d
marcusseabra <seabra.marcus@gmail.com>
2021-03-02 19:46:00 -0300

    Remocao de pasta base_valuation

--- bd_mercado/Aulas/ReadExcel.R
+++
@@ -1,9 +0,0 @@
-library(readxl)
-
-dados = read_excel("Dados/carteira.xlsx", sheet = "carteira2")
-
-dados = as.data.frame(dados)
-dados$data = as.Date(dados$data)
-dados$vencimento = as.Date(dados$vencimento)
-
-
