b947c81d7831632949155f56da4b5c0d5713cbce
marcusseabra <seabra.marcus@gmail.com>
2021-03-12 05:45:26 -0300

    Scripts para leitura pdf

---
+++ pos_graduacao/python/leitura_pdf.py
@@ -0,0 +1,34 @@
+# https://automatetheboringstuff.com/chapter13/
+# https://towardsdatascience.com/how-to-extract-text-from-pdf-245482a96de7
+# https://pdfminersix.readthedocs.io/en/latest/tutorial/composable.html
+
+# https://github.com/jsvine/pdfplumber
+
+
+from pdfminer3.layout import LAParams, LTTextBox
+from pdfminer3.pdfpage import PDFPage
+from pdfminer3.pdfinterp import PDFResourceManager
+from pdfminer3.pdfinterp import PDFPageInterpreter
+from pdfminer3.converter import PDFPageAggregator
+from pdfminer3.converter import TextConverter
+import io
+
+resource_manager = PDFResourceManager()
+fake_file_handle = io.StringIO()
+converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
+page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+with open('/home/seabra/dev/udacity/modulo-1/util/entrada/Nota_Negociacao_2020-06-26.pdf', 'rb') as fh:
+
+    for page in PDFPage.get_pages(fh,
+                                  caching=True,
+                                  check_extractable=True):
+        page_interpreter.process_page(page)
+
+    text = fake_file_handle.getvalue()
+
+# close open handles
+converter.close()
+fake_file_handle.close()
+
+print(text)
---
+++ pos_graduacao/python/leitura_pdf_v2.py
@@ -0,0 +1,24 @@
+import pdfplumber
+
+
+def leitura_arquivos_pdf():
+    # pdfplumber.open retorna instância da classe pdfplumber.PDF
+    with pdfplumber.open("/home/seabra/Documentos/Pessoal/Investimentos/Extrato_XP.pdf") as pdf:
+        primeira_pagina = pdf.pages[0]
+        print(primeira_pagina.chars[0])
+
+        metadados = pdf.metadata
+        print(metadados)
+
+        # print(primeira_pagina.extract_text(x_tolerance=2, y_tolerance=2))
+
+    with pdfplumber.open("/home/seabra/Documentos/Pessoal/Investimentos/Nota_Negociacao_2020-08-05.pdf", password = "027") as pdf:
+        primeira_pagina = pdf.pages[0]
+        print(primeira_pagina.chars[0])
+
+        print(primeira_pagina.extract_text(x_tolerance=2, y_tolerance=2))
+        # print(primeira_pagina.extract_tables())
+        print(primeira_pagina.find_tables())
+
+
+leitura_arquivos_pdf()
